d969f189811deaee684f46b087c4f999883f1197
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-12-06 12:57:44 -0300

    feat: adiciona requisito 05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -26,4 +26,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        sys.stdout.write(str(instance.search(position)))
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
