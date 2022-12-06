208941908157bf7aeafb6a9cc2927a0f8e77fdac
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-12-06 13:27:34 -0300

    feat: adiciona requisito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(instance.__len__()):
+        data = instance.search(index)
+        data_lines = data["linhas_do_arquivo"]
+        ocurrences = []
+        for j in range(data_lines.__len__()):
+            if word.lower() in data_lines[j].lower():
+                ocurrences.append(j + 1)
+        if len(ocurrences) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [
+                    {'linha': row} for row in ocurrences],
+            })
+    return result
 
 
 def search_by_word(word, instance):
