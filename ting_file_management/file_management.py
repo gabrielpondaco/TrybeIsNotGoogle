53afa5675c4d5de31f07479553024936e404cdb3
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-12-06 12:29:11 -0300

    feat: adiciona requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+    else:
+        try:
+            with open(path_file) as file:
+                data = file.read()
+                return data.splitlines()
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
