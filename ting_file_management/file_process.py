240441aad6d387e4dd2c01efb59c353767a51501
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-12-06 12:50:49 -0300

    feat: adiciona requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    file_to_list = txt_importer(path_file)
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_to_list),
+        "linhas_do_arquivo": file_to_list
+    }
+    instance.enqueue(output)
+    sys.stdout.write(f"{output}")
 
 
 def remove(instance):
